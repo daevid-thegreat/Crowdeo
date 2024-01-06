a1efff3a48894cfc9edd995f65711cbe7d0b9074
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-01-06 15:11:48 +0100

    connect eth wallet

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,10 +1,54 @@
+"use client";
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link,} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button,} from "@nextui-org/react";
 import Image from "next/image";
+import {Web3} from "web3";
 
 export default function Nav() {
+    const [isConnected, setIsConnected] = React.useState(false);
+    const [userAccount, setUserAccount] = React.useState(null);
+    const detectCurrentProvider = async () => {
+        try {
+        if (window.ethereum) {
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
+          return window.ethereum;
+        } else if (window.web3) {
+          const provider = window.web3.currentProvider;
+          return provider;
+        } else {
+          console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+          return null;
+        }
+        } catch (error) {
+        console.error('Error detecting Ethereum provider:', error);
+        return null;
+        }
+};
+
+   const onConnect = async () => {
+      try {
+        const currentProvider = await detectCurrentProvider();
+        if (currentProvider) {
+          const web3Instance = new Web3(currentProvider);
+          const accounts = await web3Instance.eth.getAccounts();
+          if (accounts.length > 0) {
+            const userAccount = accounts[0];
+            setUserAccount(userAccount);
+            setIsConnected(true);
+          } else {
+            console.log('No Ethereum accounts found.');
+          }
+        }
+      } catch (error) {
+        console.error('Error connecting to wallet:', error);
+      }
+};
+
+    const onDisconnect = () => {
+        setIsConnected(false);
+    };
   return (
-    <Navbar class={"bg-white px-24 py-4 flex justify-around"}>
+    <Navbar className={"bg-white px-10 py-4 flex justify-around"}>
       <NavbarBrand>
         <Image src={'/logo.png'} alt={'logo'} width={120} height={60} />
       </NavbarBrand>
@@ -32,8 +76,10 @@ export default function Nav() {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden py-2 px-6 rounded-lg lg:flex bg-blue-600 ">
-          <Link className={"text-white"} href="#">Login</Link>
+        <NavbarItem className="ml-10">
+               <Button onClick={isConnected ? onDisconnect : onConnect}>
+      {isConnected ? `Disconnect ${userAccount}` : 'Connect Wallet'}
+    </Button>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
