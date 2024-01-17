4a781ed983902b5adf4d6d412d40a1edd8c2f904
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-01-17 15:00:38 +0100

    updated company card

--- Components/CompanyCard.jsx
+++ Components/CompanyCard.jsx
@@ -3,18 +3,18 @@ import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nex
 
 export default function CompanyCard() {
   return (
-    <Card className="max-w-[400px]">
-      <CardHeader className="flex gap-3">
-        <div className="flex flex-col">
-          <p className="text-md">NextUI</p>
-          {/*<p className="text-small text-default-500">nextui.org</p>*/}
-        </div>
-      </CardHeader>
-      <Divider/>
-      <CardBody>
-        <p>Make beautiful websites regardless of your design experience.</p>
-      </CardBody>
-      <Divider/>
-    </Card>
+      <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+              <div className="flex flex-col">
+                  <p className="text-md">NextUI</p>
+              </div>
+          </CardHeader>
+          <Divider/>
+          <CardBody>
+              <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+          <Divider/>
+          <Link className="text-small text-default-500 p-4" href={"/yguhijop[p"}>View</Link>
+      </Card>
   );
 }
