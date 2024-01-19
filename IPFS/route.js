5205d1dc7b1d28e30c0334a3af448354e8fe6def
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-01-19 20:00:12 +0100

    working with helia...

--- IPFS/route.js
+++ IPFS/route.js
@@ -1,36 +1,31 @@
 // pages/api/store.js
 import { json } from '@helia/json';
 import { createHelia } from 'helia';
+import { NextResponse} from "next/server";
 
 const helia = createHelia();
 const j = json(helia);
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
+export async function POST(req, res) {
+    const body = await req.json();
     try {
-      const cid = await j.put(req.body); // Assuming JSON data is sent in the request body
-      res.status(200).json({ success: true, cid });
+      const cid = await j.put(body); // Assuming JSON data is sent in the request body
+     return NextResponse.json({ success: true, cid })
     } catch (error) {
       console.error(error);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
+        return NextResponse.json({ success: false, error: 'Internal Server Error' })
     }
-  } else {
-    res.status(405).json({ success: false, error: 'Method Not Allowed' });
-  }
 }
 
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
+export async function GET() {
+    const body = await req.json();
     try {
-      const { cid } = req.query;
+      const { cid } = body;
       const obj = await j.get(cid);
-      res.status(200).json({ success: true, data: obj });
+        return NextResponse.json({ success: true, obj })
     } catch (error) {
       console.error(error);
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
+        return NextResponse.json({ success: false, error: 'Internal Server Error' })
     }
-  } else {
-    res.status(405).json({ success: false, error: 'Method Not Allowed' });
-  }
 }
