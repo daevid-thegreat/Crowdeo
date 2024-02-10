9d788311ab23261a32117875b14e0cab0794540f
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-02-10 22:33:19 +0100

    adding dataset test

--- scripts/TestSmartContract.js
+++ scripts/TestSmartContract.js
@@ -1,5 +1,39 @@
-import rewardUserForReview from "./SendReward";
+// Import your JSON dataset
+// Import the necessary functions and contract
 
-export default async function createMassReview(){
-    
+import { getUserBalance, rewardUserForReview } from "./SendReward"; // Adjust the path as necessary
+
+import data from "./data.json"; // Adjust the path as necessary
+
+async function createMassReview(){
+    const prisma = new PrismaClient();
+ 
+
+// Loop through each entry in the JSON data and execute rewardUserForReview function
+data.forEach(async (entry) => {
+  const { userAddress, comment, rating, companyId } = entry;
+
+  await prisma.review.create({
+    data: {
+      rating: rating,
+      reviewerAddress: userAddress,
+      comment: comment,
+      company: {
+        connect: {
+          id: body.companyId,
+        },
+      },
+    },
+  }).then(async () => {
+    // Execute the rewardUserForReview function for each entry
+    rewardUserForReview(userAddress)
+  });
+
+    // Log the user's balance after the reward
+  const userBalance = await getUserBalance(userAddress);
+  console.log(`User ${userAddress} balance is ${userBalance} CDT tokens.`);
+});
+
 }
+
+createMassReview();
